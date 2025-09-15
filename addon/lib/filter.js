53c4b1a6234a2e30a795361867e97342624e3819
fungamefunk <88925426+fungamefunk@users.noreply.github.com>
2025-09-15 16:48:56 +0530

    Update filter.js

--- addon/lib/filter.js
+++ addon/lib/filter.js
@@ -1,240 +1,171 @@
-import { extractProvider, parseSize, extractSize } from './titleHelper.js';
-import { Type } from './types.js';
-export const Providers = {
-  key: 'providers',
+const { extractProvider, parseSize, extractSize } = require("./titleHelper.js");
+const { Type } = require("./types.js");
+
+const Providers = {
+  key: "providers",
   options: [
-    {
-      key: 'yts',
-      label: 'YTS'
-    },
-    {
-      key: 'eztv',
-      label: 'EZTV'
-    },
-    {
-      key: 'rarbg',
-      label: 'RARBG'
-    },
-    {
-      key: '1337x',
-      label: '1337x'
-    },
-    {
-      key: 'thepiratebay',
-      label: 'ThePirateBay'
-    },
-    {
-      key: 'kickasstorrents',
-      label: 'KickassTorrents'
-    },
-    {
-      key: 'torrentgalaxy',
-      label: 'TorrentGalaxy'
-    },
-    {
-      key: 'magnetdl',
-      label: 'MagnetDL'
-    },
-    {
-      key: 'horriblesubs',
-      label: 'HorribleSubs',
-      anime: true
-    },
-    {
-      key: 'nyaasi',
-      label: 'NyaaSi',
-      anime: true
-    },
-    {
-      key: 'tokyotosho',
-      label: 'TokyoTosho',
-      anime: true
-    },
-    {
-      key: 'anidex',
-      label: 'AniDex',
-      anime: true
-    },
-    {
-      key: 'rutor',
-      label: 'Rutor',
-      foreign: '🇷🇺'
-    },
-    {
-      key: 'rutracker',
-      label: 'Rutracker',
-      foreign: '🇷🇺'
-    },
-    {
-      key: 'comando',
-      label: 'Comando',
-      foreign: '🇵🇹'
-    },
-    {
-      key: 'bludv',
-      label: 'BluDV',
-      foreign: '🇵🇹'
-    },
-    {
-      key: 'torrent9',
-      label: 'Torrent9',
-      foreign: '🇫🇷'
-    },
-    {
-      key: 'ilcorsaronero',
-      label: 'ilCorSaRoNeRo',
-      foreign: '🇮🇹'
-    },
-    {
-      key: 'mejortorrent',
-      label: 'MejorTorrent',
-      foreign: '🇪🇸'
-    },
-    {
-      key: 'wolfmax4k',
-      label: 'Wolfmax4k',
-      foreign: '🇪🇸'
-    },
-    {
-      key: 'cinecalidad',
-      label: 'Cinecalidad',
-      foreign: '🇲🇽'
-    },
-    {
-      key: 'besttorrents',
-      label: 'BestTorrents',
-      foreign: '🇵🇱'
-    },
-  ]
+    { key: "yts", label: "YTS" },
+    { key: "eztv", label: "EZTV" },
+    { key: "rarbg", label: "RARBG" },
+    { key: "1337x", label: "1337x" },
+    { key: "thepiratebay", label: "ThePirateBay" },
+    { key: "kickasstorrents", label: "KickassTorrents" },
+    { key: "torrentgalaxy", label: "TorrentGalaxy" },
+    { key: "magnetdl", label: "MagnetDL" },
+    { key: "horriblesubs", label: "HorribleSubs", anime: true },
+    { key: "nyaasi", label: "NyaaSi", anime: true },
+    { key: "tokyotosho", label: "TokyoTosho", anime: true },
+    { key: "anidex", label: "AniDex", anime: true },
+    { key: "rutor", label: "Rutor", foreign: "🇷🇺" },
+    { key: "rutracker", label: "Rutracker", foreign: "🇷🇺" },
+    { key: "comando", label: "Comando", foreign: "🇵🇹" },
+    { key: "bludv", label: "BluDV", foreign: "🇵🇹" },
+    { key: "torrent9", label: "Torrent9", foreign: "🇫🇷" },
+    { key: "ilcorsaronero", label: "ilCorSaRoNeRo", foreign: "🇮🇹" },
+    { key: "mejortorrent", label: "MejorTorrent", foreign: "🇪🇸" },
+    { key: "wolfmax4k", label: "Wolfmax4k", foreign: "🇪🇸" },
+    { key: "cinecalidad", label: "Cinecalidad", foreign: "🇲🇽" },
+    { key: "besttorrents", label: "BestTorrents", foreign: "🇵🇱" },
+  ],
 };
-export const QualityFilter = {
-  key: 'qualityfilter',
+
+const QualityFilter = {
+  key: "qualityfilter",
   options: [
     {
-      key: 'brremux',
-      label: 'BluRay REMUX',
+      key: "brremux",
+      label: "BluRay REMUX",
       test(quality, bingeGroup) {
         return bingeGroup?.includes(this.label);
-      }
+      },
     },
     {
-      key: 'hdrall',
-      label: 'HDR/HDR10+/Dolby Vision',
-      items: ['HDR', 'HDR10+', 'DV'],
+      key: "hdrall",
+      label: "HDR/HDR10+/Dolby Vision",
+      items: ["HDR", "HDR10+", "DV"],
       test(quality) {
-        const hdrProfiles = quality?.split(' ')?.slice(1)?.join() || '';
-        return this.items.some(hdrType => hdrProfiles.includes(hdrType));
-      }
+        const hdrProfiles = quality?.split(" ")?.slice(1)?.join() || "";
+        return this.items.some((hdrType) => hdrProfiles.includes(hdrType));
+      },
     },
     {
-      key: 'dolbyvision',
-      label: 'Dolby Vision',
+      key: "dolbyvision",
+      label: "Dolby Vision",
       test(quality) {
-        const hdrProfiles = quality?.split(' ')?.slice(1)?.join() || '';
-        return hdrProfiles === 'DV';
-      }
+        const hdrProfiles = quality?.split(" ")?.slice(1)?.join() || "";
+        return hdrProfiles === "DV";
+      },
     },
     {
-      key: 'dolbyvisionwithhdr',
-      label: 'Dolby Vision + HDR',
+      key: "dolbyvisionwithhdr",
+      label: "Dolby Vision + HDR",
       test(quality) {
-        const hdrProfiles = quality?.split(' ')?.slice(1)?.join() || '';
-        return hdrProfiles.includes('DV') && hdrProfiles.includes('HDR');
-      }
+        const hdrProfiles = quality?.split(" ")?.slice(1)?.join() || "";
+        return hdrProfiles.includes("DV") && hdrProfiles.includes("HDR");
+      },
     },
     {
-      key: 'threed',
-      label: '3D',
+      key: "threed",
+      label: "3D",
       test(quality) {
-        const hdrProfiles = quality?.split(' ')?.slice(1)?.join() || '';
-        return hdrProfiles.includes('3D');
-      }
+        const hdrProfiles = quality?.split(" ")?.slice(1)?.join() || "";
+        return hdrProfiles.includes("3D");
+      },
     },
     {
-      key: 'nonthreed',
-      label: 'Non 3D (DO NOT SELECT IF NOT SURE)',
+      key: "nonthreed",
+      label: "Non 3D (DO NOT SELECT IF NOT SURE)",
       test(quality) {
-        const hdrProfiles = quality?.split(' ')?.slice(1)?.join() || '';
-        return !hdrProfiles.includes('3D');
-      }
+        const hdrProfiles = quality?.split(" ")?.slice(1)?.join() || "";
+        return !hdrProfiles.includes("3D");
+      },
     },
     {
-      key: '4k',
-      label: '4k',
-      items: ['4k'],
+      key: "4k",
+      label: "4k",
+      items: ["4k"],
       test(quality) {
-        return quality && this.items.includes(quality.split(' ')[0]);
-      }
+        return quality && this.items.includes(quality.split(" ")[0]);
+      },
     },
     {
-      key: '1080p',
-      label: '1080p',
-      items: ['1080p'],
+      key: "1080p",
+      label: "1080p",
+      items: ["1080p"],
       test(quality) {
-        return this.items.includes(quality)
-      }
+        return this.items.includes(quality);
+      },
     },
     {
-      key: '720p',
-      label: '720p',
-      items: ['720p'],
+      key: "720p",
+      label: "720p",
+      items: ["720p"],
       test(quality) {
-        return this.items.includes(quality)
-      }
+        return this.items.includes(quality);
+      },
     },
     {
-      key: '480p',
-      label: '480p',
-      items: ['480p'],
+      key: "480p",
+      label: "480p",
+      items: ["480p"],
       test(quality) {
-        return this.items.includes(quality)
-      }
+        return this.items.includes(quality);
+      },
     },
     {
-      key: 'other',
-      label: 'Other (DVDRip/HDRip/BDRip...)',
-      // could be ['DVDRip', 'HDRip', 'BDRip', 'BRRip', 'BluRay', 'WEB-DL', 'WEBRip', 'HDTV', 'DivX', 'XviD']
-      items: ['4k', '1080p', '720p', '480p', 'SCR', 'CAM', 'TeleSync', 'TeleCine'],
+      key: "other",
+      label: "Other (DVDRip/HDRip/BDRip...)",
+      items: [
+        "4k",
+        "1080p",
+        "720p",
+        "480p",
+        "SCR",
+        "CAM",
+        "TeleSync",
+        "TeleCine",
+      ],
       test(quality) {
-        return quality && !this.items.includes(quality.split(' ')[0]);
-      }
+        return quality && !this.items.includes(quality.split(" ")[0]);
+      },
     },
     {
-      key: 'scr',
-      label: 'Screener',
-      items: ['SCR'],
+      key: "scr",
+      label: "Screener",
+      items: ["SCR"],
       test(quality) {
-        return this.items.includes(quality)
-      }
+        return this.items.includes(quality);
+      },
     },
     {
-      key: 'cam',
-      label: 'Cam',
-      items: ['CAM', 'TeleSync', 'TeleCine'],
+      key: "cam",
+      label: "Cam",
+      items: ["CAM", "TeleSync", "TeleCine"],
       test(quality) {
-        return this.items.includes(quality)
-      }
+        return this.items.includes(quality);
+      },
     },
     {
-      key: 'unknown',
-      label: 'Unknown',
+      key: "unknown",
+      label: "Unknown",
       test(quality) {
-        return !quality
-      }
-    }
-  ]
+        return !quality;
+      },
+    },
+  ],
 };
-export const SizeFilter = {
-  key: 'sizefilter'
-}
-const defaultProviderKeys = Providers.options.map(provider => provider.key);
 
-export default function applyFilters(streams, config) {
-  return [
-    filterByProvider,
-    filterByQuality,
-    filterBySize
-  ].reduce((filteredStreams, filter) => filter(filteredStreams, config), streams);
+const SizeFilter = {
+  key: "sizefilter",
+};
+
+const defaultProviderKeys = Providers.options.map((provider) => provider.key);
+
+function applyFilters(streams, config) {
+  return [filterByProvider, filterByQuality, filterBySize].reduce(
+    (filteredStreams, filter) => filter(filteredStreams, config),
+    streams
+  );
 }
 
 function filterByProvider(streams, config) {
@@ -242,10 +173,10 @@ function filterByProvider(streams, config) {
   if (!providers?.length) {
     return streams;
   }
-  return streams.filter(stream => {
+  return streams.filter((stream) => {
     const provider = extractProvider(stream.title).toLowerCase();
     return providers.includes(provider);
-  })
+  });
 }
 
 function filterByQuality(streams, config) {
@@ -253,11 +184,15 @@ function filterByQuality(streams, config) {
   if (!filters) {
     return streams;
   }
-  const filterOptions = QualityFilter.options.filter(option => filters.includes(option.key));
-  return streams.filter(stream => {
-    const streamQuality = stream.name.split('\n')[1];
+  const filterOptions = QualityFilter.options.filter((option) =>
+    filters.includes(option.key)
+  );
+  return streams.filter((stream) => {
+    const streamQuality = stream.name.split("\n")[1];
     const bingeGroup = stream.behaviorHints?.bingeGroup;
-    return !filterOptions.some(option => option.test(streamQuality, bingeGroup));
+    return !filterOptions.some((option) =>
+      option.test(streamQuality, bingeGroup)
+    );
   });
 }
 
@@ -266,9 +201,18 @@ function filterBySize(streams, config) {
   if (!sizeFilters?.length) {
     return streams;
   }
-  const sizeLimit = parseSize(config.type === Type.MOVIE ? sizeFilters.shift() : sizeFilters.pop());
-  return streams.filter(stream => {
-    const size = extractSize(stream.title)
+  const sizeLimit = parseSize(
+    config.type === Type.MOVIE ? sizeFilters.shift() : sizeFilters.pop()
+  );
+  return streams.filter((stream) => {
+    const size = extractSize(stream.title);
     return size <= sizeLimit;
-  })
+  });
 }
+
+module.exports = {
+  Providers,
+  QualityFilter,
+  SizeFilter,
+  applyFilters,
+};
